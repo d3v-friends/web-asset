e5da882caf5a11a9a1f5c1e65a1253334a3e682d
ciaolee <ciaolee@d3v-friends.com>
2025-05-30 20:09:32 +0900

    update build script

--- script/tag.sh
+++ script/tag.sh
@@ -1,18 +1,11 @@
 #!/bin/zsh
 set -e;
-echo "$1";
-exit 1;
 
 BASE_TAG=$1
 TAG="$BASE_TAG"
 ORIGIN="origin"
 
 echo "tag=$TAG, base_tag=$BASE_TAG, origin=$ORIGIN"
-
-git tag -d "$TAG"
-git push -d "$ORIGIN" "$TAG"
-git tag -d latest
-git push -d latest
 
 git tag "$TAG"
 git push --tags "$ORIGIN" "$TAG"
